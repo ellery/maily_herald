fc86be1cda87b83411d8bc107bf34af051b845e3
Ellery Durgin <ejd91717@gmail.com>
2016-12-19 14:40:48 -0500

    Fix Bug in MaileyHarald::Manager.job_enqueued? using a assignment rather then a camparison operator in SideKiq::RetrySet

--- lib/maily_herald/manager.rb
+++ lib/maily_herald/manager.rb
@@ -29,9 +29,9 @@ module MailyHerald
 
     # Check if Maily sidekiq job is running.
     def self.job_enqueued?
-      Sidekiq::Queue.new.detect{|j| j.klass == "MailyHerald::Async" } || 
+      Sidekiq::Queue.new.detect{|j| j.klass == "MailyHerald::Async" } ||
         Sidekiq::Workers.new.detect{|w, msg| msg["payload"]["class"] == "MailyHerald::Async" } ||
-        Sidekiq::RetrySet.new.detect{|j| j.klass = "MailyHerald::Async" }
+        Sidekiq::RetrySet.new.detect{|j| j.klass == "MailyHerald::Async" }
     end
   end
 end
